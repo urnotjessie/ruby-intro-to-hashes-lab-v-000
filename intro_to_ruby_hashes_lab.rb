a3ae0c1c4e798db318f83598a06dfe37abd2a402
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:36:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,11 +58,11 @@ def monopoly_with_fourth_tier
 	monopoly[:railroads][:names][:reading_railroad] = {}
 	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 	monopoly[:railroads][:names][:b_and_o_railroad] = {}
-	monopoly[:railroads][:names][:shortline] = {}
-	monopoly_with_third_tier[:railroads][:names][:reading_railroad]['mortgage_value'] = "$100"
-	monopoly_with_third_tier[:railroads][:names][:pennsylvania_railroad]['mortgage_value'] = "$200"
-	monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad]['mortgage_value'] = "$400"
-	monopoly_with_third_tier[:railroads][:names][:shortline]['mortgage_value'] = "$800"
+	monopoly[:railroads][:names][:shortline] = {
+	monopoly[:railroads][:names][:reading_railroad]['mortgage_value'] = "$100"
+	monopoly[:railroads][:names][:pennsylvania_railroad]['mortgage_value'] = "$200"
+	monopoly[:railroads][:names][:b_and_o_railroad]['mortgage_value'] = "$400"
+	monopoly[:railroads][:names][:shortline]['mortgage_value'] = "$800"
 	monopoly
 
 end
