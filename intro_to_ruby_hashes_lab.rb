9d5ef848537cbbc6cd45e6f00e857fe932f258de
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:32:43 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,5 +43,6 @@ def monopoly_with_fourth_tier
 	monopoly_with_third_tier[:railroads][:names][:pennsylvania_railroad]['mortgage_value'] = "$200"
 	monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad]['mortgage_value'] = "$400"
 	monopoly_with_third_tier[:railroads][:names][:shortline]['mortgage_value'] = "$800"
+	monopoly
 
 end
