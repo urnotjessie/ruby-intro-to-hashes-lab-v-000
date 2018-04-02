7a809f82816496ca378f8ed62f5b938ca32f0c8e
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:24:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,7 @@ def monopoly_with_third_tier
 	monopoly_with_second_tier[:railroads][:rent_in_dollars][:one_piece_owned] = 25
 	monopoly_with_second_tier[:railroads][:rent_in_dollars][:two_piece_owned] = 50
 	monopoly_with_second_tier[:railroads][:rent_in_dollars][:three_piece_owned] = 100
+	monopoly_with_second_tier[:railroads][:rent_in_dollars][:four_piece_owned] = 200
 end
 
 def monopoly_with_fourth_tier
