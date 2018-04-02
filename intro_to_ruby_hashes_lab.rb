93860a7985f2ecc567d6324f12fb27d12e779aba
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:33:26 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_second_tier
 	monopoly
 
 end
+binding.pry
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
